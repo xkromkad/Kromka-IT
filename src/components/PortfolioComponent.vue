dd5e80edd4b25e1dbaaee451dfd4ded3926511a6
Dávid Kromka <xkromkad@stuba.sk>
2024-11-07 17:28:29 +0100

    a: portfolio items

--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -24,6 +24,60 @@ export default defineComponent({
     const webmSource = laptop;
     const videoElement = ref<HTMLVideoElement | null>(null);
     const hasPlayed = ref(false);
+    const portfolioItems = [
+      {
+        title: 'Sql Injection Lab',
+        type: 'Web',
+        description:
+          'Laboratórium na bezpečné a legálne trénovanie svojich zručností v útokoch SQL Injection.!',
+        notes: ['SQL Injection', 'Informačná bezpečnosť', 'Vzdelávanie'],
+        projectUrl: 'https://sqlinjectionlab.kromka.it/',
+        githubUrl: 'https://github.com/xkromkad/SQLInjectionLab',
+        image: '',
+      },
+      {
+        title: 'Mirka a Dávid',
+        type: 'Web',
+        description:
+          'Svadobná stránka s informáciami pre hostí a možnosťou nahrávať fotky zo svadby pre svadobčanov. Svadbu sme si užili, len sa bežte pozrieť!',
+        notes: [
+          'Svadobná stránka',
+          'Informácie pre hostí',
+          'Zdieľanie fotografií',
+        ],
+        projectUrl: 'https://mirkaadavid.kromka.it/',
+        githubUrl: 'https://github.com/xkromkad/mirkaadavid',
+        image: '',
+      },
+      {
+        title: 'Toxipred - tímová stránka',
+        type: 'Web',
+        description:
+          'Reprezentatívna stránka tímu do súťaže TP CUP 2023/2024 na FIIT STU. Cieľom projektu bolo vytvoriť predikčný model na odhaľovanie genotoxicity látok.',
+        notes: [
+          'Školský projekt',
+          'Reprezentatívna stránka tímu',
+          'Umelá inteligencia',
+        ],
+        projectUrl: 'https://toxipred.kromka.it',
+        githubUrl: 'https://github.com/xkromkad/Toxipred',
+        image: '',
+      },
+      {
+        title: 'Toxipred - predikcia genotoxicity',
+        type: 'Web',
+        description:
+          'Voľne dostupné nástroje na predikciu fototoxicity a genotoxicity látok pomocou strojového učenia. Projekt vznikol v spolupráci 2 tímov v súťaži TP CUP na FIIT STU. Bol som jedným z lenov tímu, ktorý sa zaoberal predikciou genotoxicity.',
+        notes: [
+          'Školský projekt',
+          'Predikcia genotoxicity',
+          'Umelá inteligencia',
+        ],
+        projectUrl: 'https://mltox.fiit.stuba.sk',
+        githubUrl: '',
+        image: '',
+      },
+    ];
 
     onMounted(() => {
       if (videoElement.value) {
